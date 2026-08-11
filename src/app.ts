1302fa781d7093b1e8cdcfc65aad7866b712f75d
Seu Nome <github.com/wesleyalvesoliveira-cmd>
2026-08-11 10:53:50 -0300

    api

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+// Importa a biblioteca Express e também o tipo Express
+// O Express será utilizado para criar o servidor web
+import express from "express";
+import type { Express } from "express";
+
+// Cria uma aplicação Express
+// A função express() devolve um objeto que representa o servidor da aplicação
+const app: Express = express();
+
+// Define a porta onde o servidor ficará disponível
+// Neste caso, o servidor poderá ser acessado pela porta 8081
+const PORT: number = 8081;
+
+// Inicializa o servidor utilizando a porta definida
+// O método listen() faz o servidor começar a "escutar" requisições HTTP
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
